c3a5a3323d3ee22aba1b9984f5636716dce9b7a3
llj <1418515165@qq.com>
2021-02-13 15:22:09 +0800

    init takeout

---
+++ src/main.js
@@ -0,0 +1,13 @@
+/**
+ * 入口js
+ */
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  router
+})
